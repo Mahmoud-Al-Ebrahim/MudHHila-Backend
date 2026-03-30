a614e1d135b1f0bf7dfe5b198e30d36a71fee4d6
Mahmoud AlEbrahim <ma453935@gmail.com>
2026-03-30 18:19:22 +0300

    fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     loginUser: async (req, res) => {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const phoneRegex = /^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/;
 
-        if (!emailRegex.test(req.body.email)) {
-            return res.status(400).json({ status: false, message: "البريد الالكتروني المدخل غير صالح" });
+        if (!phoneRegex.test(req.body.phone)) {
+            return res.status(400).json({ status: false, message: "رقم الهاتف المدخل غير صالح" });
         }
 
        if (req.body.password < 8) {
@@ -15,23 +15,23 @@ module.exports = {
         }
 
         try {
-            const user = await User.findOne({ email: req.body.email });
+            const user = await User.findOne({ phone: req.body.phone });
 
             if (!user) {
-                return res.status(400).json({ status: false, message: "البريد الالكتروني أو كلمة المرور غير صحيحة" });
+                return res.status(400).json({ status: false, message: "رقم الهاتف أو كلمة المرور غير صحيحة" });
             }
 
             const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET);
              const depassword = decryptedPassword.toString(CryptoJS.enc.Utf8);
 
             if (depassword !== req.body.password) {
-                return res.status(400).json({ status: false, message: "البريد الالكتروني أو كلمة المرور غير صحيحة" });
+                return res.status(400).json({ status: false, message: "رقم الهاتف أو كلمة المرور غير صحيحة" });
             }
             const userToken = jwt.sign({
                 id: user._id,
-                email: user.email,
-                name: user.name
-            }, process.env.JWT_SECRET, { expiresIn: "21d" });
+                phone: user.phone,
+                name: user.name,
+            }, process.env.JWT_SECRET);
 
 
             res.status(200).json({token: userToken , user: user});
@@ -42,10 +42,10 @@ module.exports = {
     },
 
         createUser: async (req, res) => {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const phoneRegex = /^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/;
 
-        if (!emailRegex.test(req.body.email)) {
-            return res.status(400).json({ status: false, message: "البريد الالكتروني المدخل غير صالح" });
+        if (!phoneRegex.test(req.body.phone)) {
+            return res.status(400).json({ status: false, message: "رقم الهاتف المدخل غير صالح" });
         }
 
         const minPasswordLength = 8;
@@ -55,27 +55,28 @@ module.exports = {
         }
 
         try {
-            const emailExists = await User.findOne({ email: req.body.email });
+            const phoneExists = await User.findOne({ phone: req.body.phone });
 
-            if (emailExists) {
-                return res.status(400).json({ status: false, message: "البريد الالكتروني موجود مسبقا" });
+            if (phoneExists) {
+                return res.status(400).json({ status: false, message: "رقم الهاتف موجود مسبقا" });
             }
 
             const newUser = new User({
                 username: req.body.username,
-                email: req.body.email,
+                phone: req.body.phone,
                 name: req.body.name,
+                ut: "مستخدم",
                 password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET).toString(),
-            })
+            });
 
             //SAVE USER
             await newUser.save();
 
          const userToken = jwt.sign({
                 id: newUser._id,
-                email: newUser.email,
+                phone: newUser.phone,
                 name: newUser.name
-            }, process.env.JWT_SECRET, { expiresIn: "21d" });
+            }, process.env.JWT_SECRET);
             res.status(201).json({ status: true, message: "تم إنشاء الحساب بنجاح" ,token: userToken , user: newUser});
         } catch (error) {
             res.status(500).json({ status: false, message: error.message });
